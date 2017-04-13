cb2ba5794448d50b5bc54b2cf1d4609efd79b84e
Satan Love <panmihaurkin@icloud.com>
2017-04-13 19:14:31 +0700

    Added string test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
+/*
+	Javascript Test Environment
+	
+	(test functions realization
+	only)
+*/
+
 var output = document.getElementById("output");
 
-function assert(outcome, description) 
+function ASSERT_GENERAL(outcome, description) 
 {
 	var li = document.createElement("li");
 	li.className = outcome ? "pass" : "fail";
@@ -8,6 +15,58 @@ function assert(outcome, description)
 
 	output.appendChild(li);
 }; 
+
+function ASSERT_STR(exp, res)
+{
+	var pass_count = 0;
+
+	if(exp.length != res.length) return false
+
+	for(var i = 0; i < exp.length; i++) {
+		for(var j = 0; j < res.length; j++) {
+			if (exp[i] === res[j]) {
+				pass_count++
+				break;
+			}
+		}
+	}
+	return pass_count == exp.length ? true : false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
