d7a5d582b25b64445ec9f575d33124e99dad2739
Satan Love <panmihaurkin@icloud.com>
2017-05-20 15:14:07 +0700

    Ready tests specs environment

--- test/test.js
+++ test/test.js
@@ -1,37 +1,11 @@
 /*
 	Javascript Test Environment
 	
-	(test functions realization
+	(test functions specs
 	only)
 */
 
-var output = document.getElementById("output");
 
-function ASSERT_GENERAL(outcome, description) 
-{
-	var li = document.createElement("li");
-	li.className = outcome ? "pass" : "fail";
-	li.appendChild(document.createTextNode(description));
-
-	output.appendChild(li);
-}; 
-
-function ASSERT_STR(exp, res)
-{
-	var pass_count = 0;
-
-	if(exp.length != res.length) return false
-
-	for(var i = 0; i < exp.length; i++) {
-		for(var j = 0; j < res.length; j++) {
-			if (exp[i] === res[j]) {
-				pass_count++
-				break;
-			}
-		}
-	}
-	return pass_count == exp.length ? true : false
-}
 
 
 
